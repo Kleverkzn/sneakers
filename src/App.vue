0a7996e06bd3e2b5168df069614d3194afa92be3
alexmix20021975 <Alexmix_20021975>
2024-03-19 12:17:03 +0300

    tree commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+
+
+</script>
+
+<template>
+  <div>
+<h1>
+  Shop
+</h1>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+</div>
+</template>
+
+<script setup>
+  defineProps({
+    title: String,
+    price: Number
+  })
+</script>
+
+<style scoped>
+  .product {
+    border: 1px solid blue;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+</style>
